35d3913f13a7bc98e5f3d3711cdee371769cf6da
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-05 19:56:51 -0800

    Code

---
+++ Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_Class_Constructor
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+
+        // Default constructor
+        public Book()
+        {
+            title = "Unknown";
+            author = "Unknown";
+        }
+
+        // Custom constructor with two parameters
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+
+        // Overloaded constructor with one parameter
+        public Book(string title)
+        {
+            this.title = title;
+            author = "Unknown";
+        }
+
+        // Properties for Title and Author
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+    } // class
+
+} // namespace
--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Exploring Constructors in C#");
+
+            // Creating instances using different constructors
+            Book book1 = new Book("1984", "George Orwell");
+            Console.WriteLine($"Book 1: Title = {book1.Title}, Author = {book1.Author}");
+
+            Book book2 = new Book("The Hobbit");
+            Console.WriteLine($"Book 2: Title = {book2.Title}, Author = {book2.Author}");
         }
     }
 }
